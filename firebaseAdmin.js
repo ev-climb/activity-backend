832f265c79803cd87d6506c512a7a0f325888797
e.evseev <evseev.develop@gmail.com>
2025-05-21 14:55:08 +0300

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -6,7 +6,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
-  });import serviceAccount from './serviceAccountKey.json' assert { type: 'json' };
+  });
 }
 
 const db = admin.firestore();
